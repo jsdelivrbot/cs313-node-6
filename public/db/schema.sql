01a51dbf7d772e7af5a4ef7d7227b206d1e9b57d
danielmartincraig <danielmartincraig@gmail.com>
2018-06-29 17:29:35 -0600

    fleshed out db schema

--- public/db/schema.sql
+++ public/db/schema.sql
@@ -1,5 +1,20 @@
 CREATE TABLE highscores
 ( id SERIAL
-, initial varchar(3)
-,
+, trajectory_id NUMBER FOREIGN KEY REFERENCES trajectories(id)
+, player_initials VARCHAR(3)
+, score NUMBER
+);
+
+CREATE TABLE quotes
+( id SERIAL
+, quote_text TEXT
+, quote_author TEXT
+);
+
+CREATE TABLE trajectories
+( id SERIAL
+, shot_angle NUMBER
+, shot_velocity NUMBER
+, duration NUMBER
+, final_distance NUMBER
 );
